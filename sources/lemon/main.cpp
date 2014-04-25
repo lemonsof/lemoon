67210f6e9dcb8db0823e6fd4d3142d6246aec7e3
yayanyang <yayanyang@gmail.com>
2014-04-25 14:44:52 +0800

    add lua

---
+++ sources/lemon/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @file     main
+ * @brief    Copyright (C) 2014  yayanyang All Rights Reserved
+ * @author   yayanyang
+ * @version  1.0.0.0
+ * @date     2014/04/25
+*/
+int main(int args, char**argv)
+{
+    args; argv;
+}
