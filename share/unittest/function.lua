655de4a4cf768e940b7d149b87fa42fdf37e25b8
yayanyang <yayanyang@gmail.com>
2014-05-06 11:15:08 +0800

    update
    Signed-off-by: yayanyang <yayanyang@gmail.com>

--- share/unittest/function.lua
+++
@@ -1,8 +0,0 @@
-lemon.call0()
-
-call = lemon.new_call(100)
-
-print(call:call())
-
-
-call:print(lemon.cout())
--- sources/lemon/sources/lemon/luabind/luabind.hpp
+++
@@ -1,5 +0,0 @@
-#include <lemon/luabind/ldef.hpp>
-#include <lemon/luabind/lcall.hpp>
-#include <lemon/luabind/lcast.hpp>
-#include <lemon/luabind/lstack.hpp>
-#include <lemon/luabind/lstate.hpp>
