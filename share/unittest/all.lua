8c163839fa6a2b602181acebddc89f760806202c
yayanyang <yayanyang@gmail.com>
2014-05-24 09:31:45 +0800

    update
    Signed-off-by: yayanyang <yayanyang@gmail.com>

--- share/unittest/all.lua
+++ share/unittest/all.lua
@@ -1,9 +1,5 @@
 --require "timer_test"
 --require "sockaddr_test"
 require "tcp_test"
-<<<<<<< HEAD
 require "udp_test"
 
-=======
-require "udp_test"
->>>>>>> 276be7a92a5100594dd4a182417f37d8503b142c
